6de7fe2aeed3ab5a41c6f254bbabfa2eed616682
ConTejas624 <41122318+ConTejas624@users.noreply.github.com>
2020-10-23 20:42:16 -0400

    initial commit

---
+++ bot.py
@@ -0,0 +1,23 @@
+import discord
+
+# reference links
+# discord.py API reference: https://discordpy.readthedocs.io/en/latest/api.html#
+# discord dev app portal: https://discord.com/developers/applications
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+token_file = open("resources\\token.txt", "r")  # get bot token from a file
+token = token_file.read()                       # file in .gitignore for security
+token_file.close()
+
+client.run(token)
