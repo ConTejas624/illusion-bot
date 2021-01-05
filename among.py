9f81c590b22f624ad7b931cc6b77c5b33e34ee8c
ConTejas624 <41122318+ConTejas624@users.noreply.github.com>
2021-01-04 20:29:29 -0600

    Update among.py
    Started importing libraries and began the code

--- among.py
+++ among.py
@@ -1,40 +1,59 @@
-# imports
+from PIL import Image, ImageGrab, ImageOps, PyAccess
+# https://pillow.readthedocs.io/en/stable/reference/Image.html
+
+# vision processing to determine what state the game is in
+# returns 1 if talking, 0 if silent, 2 for game end
+def get_game_state(frame):
+    if frame == 2:
+        return 1
+    return 0
+
+
+# get frame from game
+def get_frame():
+    screen = ImageGrab.grab()
+
 
 class AmongUs:
 
     def __init__(self, players):
         super().__init__()
-        self.players = players
-        pass
+        self.players = {}
+        self.running = False
 
+        for player in players:
+            self.players.update({player: True})
+
+    # game setup
     def start_game(self):
-        pass
+        self.running = True
+        self.get_colors()
 
     def end_game(self):
-        pass
+        self.running = False
 
-    def play_jester(self):
-        pass
+    def play(self):
+        self.start_game()
+        last_state = 0
 
-    def play_vanilla(self):
-        pass
+        while self.running:
+            frame = get_frame()
+            state = get_game_state(frame)
+
+            if last_state != state and state == 1:
+                # TODO: un-mute alive players
+                pass
+            elif last_state != state and state == 0:
+                # TODO: mute all players
+                pass
+            elif state == 2:
+                self.end_game()
 
     # vision processing to associate colors to players
     def get_colors(self):
         pass
 
-    # vision processing to determine what state the game is in
-    def get_game_state(self):
-        pass
-
     # vision processing to check who is dead
-    def get_dead(self):
-        pass
-
-    # vision processing to see who voted for who
-    def get_voted(self):
-        pass
-
-    # get frame from game
-    def get_frame(self):
+    # returns list of players' discord ids
+    def get_dead(self, frame):
         pass
